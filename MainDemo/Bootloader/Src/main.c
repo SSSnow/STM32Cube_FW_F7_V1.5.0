3d7a72da5deda5f01adb769d86cc5febd3a3e3f9
WSCKY <hello.kychu@gmail.com>
2016-12-09 00:59:42 +0800

    update Makefile

--- MainDemo/Bootloader/Src/main.c
+++ MainDemo/Bootloader/Src/main.c
@@ -16,9 +16,9 @@
 # define APP_LOAD_ADDRESS               0x0800C000
 
 # define BOARD_FLASH_SIZE               (2 * 1024 * 1024)
-#define BOOTLOADER_RESERVATION_SIZE	(16 * 1024)
+# define BOOTLOADER_RESERVATION_SIZE	(16 * 1024)
 # define APP_RESERVATION_SIZE		(2 * 16 * 1024) /* 2 16 Kib Sectors */
-#define APP_SIZE_MAX			(BOARD_FLASH_SIZE - (BOOTLOADER_RESERVATION_SIZE + APP_RESERVATION_SIZE))
+# define APP_SIZE_MAX			(BOARD_FLASH_SIZE - (BOOTLOADER_RESERVATION_SIZE + APP_RESERVATION_SIZE))
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 uint8_t Times = 5;
@@ -28,7 +28,7 @@ static GPIO_InitTypeDef  GPIO_InitStruct;
 void jump_to_app(void);
 static void do_jump(uint32_t stacktop, uint32_t entrypoint);
 
-void SystemClock_Config(void);
+//void SystemClock_Config(void);
 static void CPU_CACHE_Enable(void);
 static void Error_Handler(void);
 
@@ -57,7 +57,7 @@ int main(void)
   HAL_Init();
 
   /* Configure the system clock to 216 MHz */
-  SystemClock_Config();
+//  SystemClock_Config();
 
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -116,7 +116,7 @@ void jump_to_app(void)
 		return;
 	}
 	/* switch exception handlers to the application */
-	SCB->VTOR = APP_LOAD_ADDRESS;
+//	SCB->VTOR = APP_LOAD_ADDRESS;
 	/* extract the stack and entrypoint from the app vector table and go */
 	do_jump(app_base[0], app_base[1]);
 }
@@ -155,46 +155,46 @@ Error_Handler();
   * @param  None
   * @retval None
   */
-void SystemClock_Config(void)
-{
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  
-  /* Enable HSE Oscillator and activate PLL with HSE as source */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 432;  
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 9;
-  RCC_OscInitStruct.PLL.PLLR = 7;
-  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    while(1) {};
-  }
-  
-  /* Activate the OverDrive to reach the 216 Mhz Frequency */
-  if(HAL_PWREx_EnableOverDrive() != HAL_OK)
-  {
-    while(1) {};
-  }
-  
-  
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
-     clocks dividers */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
-  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
-  {
-    while(1) {};
-  }
-}
+//void SystemClock_Config(void)
+//{
+//  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+//  RCC_OscInitTypeDef RCC_OscInitStruct;
+//  
+//  /* Enable HSE Oscillator and activate PLL with HSE as source */
+//  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+//  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+//  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+//  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+//  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+//  RCC_OscInitStruct.PLL.PLLM = 16;
+//  RCC_OscInitStruct.PLL.PLLN = 432;  
+//  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+//  RCC_OscInitStruct.PLL.PLLQ = 9;
+//  RCC_OscInitStruct.PLL.PLLR = 7;
+//  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+//  {
+//    while(1) {};
+//  }
+//  
+//  /* Activate the OverDrive to reach the 216 Mhz Frequency */
+//  if(HAL_PWREx_EnableOverDrive() != HAL_OK)
+//  {
+//    while(1) {};
+//  }
+//  
+//  
+//  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+//     clocks dividers */
+//  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+//  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+//  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+//  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+//  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+//  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+//  {
+//    while(1) {};
+//  }
+//}
 /**
 * @brief  CPU L1-Cache enable.
 * @param  None
